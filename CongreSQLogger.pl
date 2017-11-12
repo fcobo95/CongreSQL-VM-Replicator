e69f70c9283ce0aac824f8d555675baf1d95e77d
fcobo95 <fcobo95@gmail.com>
2017-11-12 15:57:28 -0600

    Started to create all the appropriate nodes for the cluster to be managed with Ansible.

--- CongreSQLogger.pl
+++ CongreSQLogger.pl
@@ -6,30 +6,31 @@ use feature qw/say/;
 
 say "Enter the amount of VM's to create: ";
 my $user_vm_count = <STDIN>;
-chomp;
-say "A default main node will be created as well.\n";
-say "Creating new VM's.................\n";
+chomp $user_vm_count;
+say "A default main node will be created as well.";
+say "Creating new VM's.................";
 
-say "Creating Main Server Node............\n";
+say "Creating Main Server Node............";
 system "VBoxManage clonevm 'Ubuntu Template' --name 'Ubuntu Server Main Node' --register";
 system "VBoxManage startvm 'Ubuntu Server Main Node'";
-say "\n";
 
 for (my $vm_number = 1; $vm_number <= $user_vm_count; $vm_number++) {
 
     if (int($vm_number - 1) == 0) {
         say "Previous VM was Ubuntu Server Main Node";
     }
-    say "Previous VM was VM Ubuntu Server Node" . int($vm_number - 1) . "\n";
+
+    say "Previous VM was VM Ubuntu Server Node" . int($vm_number - 1);
     system "VBoxManage clonevm 'Ubuntu Template' --name 'Ubuntu Server Node $vm_number' --register";
-    say "Creating next VM....\n";
+    say "Creating next VM....";
 
     if ($vm_number > 5) {
         system "VBoxManage startvm 'Ubuntu Server Node $vm_number' --type headless";
     }
+
     else {
         system "VBoxManage startvm 'Ubuntu Server Node $vm_number'";
     }
 
-    say "$vm_number\n";
+    say "$vm_number";
 }
