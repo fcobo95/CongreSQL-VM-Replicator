00cbf57aab87a8958621a7b2719252d941073242
Krakenaitor <fcobo95@gmail.com>
2017-11-13 09:23:17 -0600

    Added missing links in the reference file and now attempting to create the Logger.

--- CongreSQLogger.pl
+++ CongreSQLogger.pl
@@ -2,6 +2,35 @@
 
 use strict;
 use warnings;
+use feature qw/say/;
 use DBI;
 # Importing the Database interface to be used to connect to the PostgresSQL database.
 
+say "Please enter the DB name to which you are trying to connect:";
+my $dbname = <STDIN>;
+chomp $dbname;
+
+say "Enter the host ip or localhost:";
+my $host = <STDIN>;
+chomp $host;
+
+say "Enter the port number of the DB:";
+my $port = <STDIN>;
+chomp $port;
+
+say "Enter your username:";
+my $username = <STDIN>;
+chomp $username;
+
+say "Enter your password:";
+my $password = <STDIN>;
+chomp $password;
+
+my $connection = DBI->connect("dbi:Pg:dbname=$dbname; host=$host; port=$port",
+    $username,
+    $password,
+    {
+        AutoCommit => 0, RaiseError => 1
+    }) || die $DBI::errstr;
+
+$connection->trace(1, 'tracelog.txt');
